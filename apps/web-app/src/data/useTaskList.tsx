d410115d5bc2a28c946b1945eb2a74d911341e72
Bradley Holbrook <bradley@praetorianstudio.com>
2020-11-12 19:47:08 -0800

    Integrated POST, PATCH, and GET into web-app

---
+++ apps/web-app/src/data/useTaskList.tsx
@@ -0,0 +1,70 @@
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
+import { tasks, tasks as tasksApi } from '../api/tasks';
+
+export interface ITask {
+    id: number;
+    done: boolean;
+    description: string;
+}
+
+export const taskListState = atom({
+    key: 'taskList',
+    default: [] as ITask[]
+});
+
+export const completedTaskListState = selector({
+    key: 'completedTaskList',
+    get: ({ get }) => {
+        return get(taskListState).filter(task => task.done)
+    }
+})
+
+export const remainingTaskListState = selector({
+    key: 'remainingTaskList',
+    get: ({ get }) => {
+        return get(taskListState).filter(task => !task.done)
+    }
+});
+
+export function useTaskList(){
+
+    const [ taskList, setTaskList ] = useRecoilState<ITask[]>(taskListState);
+    const completedTaskList = useRecoilValue(completedTaskListState);
+    const remainingTaskList = useRecoilValue(remainingTaskListState);
+    
+    const loadRemoteTasks = () => {
+        tasksApi.get().then( response => {
+            setTaskList( response.data );
+        });
+    }
+
+    const create = ( description:string ) => {
+        tasksApi.post({ done: false, description }).then( response => {
+            setTaskList([ ...taskList, response.data ]);
+        });
+    }
+
+    const markDone = ( id:number ) => {
+        tasksApi.patch( id, { done: true }).then( response => {
+            return tasksApi.get();
+        }).then( response => {
+            setTaskList( response.data );
+        });
+    }
+
+    const remove = () => {
+
+    }
+
+    return {
+        taskList,
+        completedTaskList,
+        remainingTaskList,
+        setTaskList,
+        loadRemoteTasks,
+        create,
+        markDone,
+        remove
+    }
+
+}
